2cc10debe2fda8d8b3bd4c71082c4776a397529b
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-07-04 16:03:48 +0600

    add header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -9,27 +9,30 @@ import cls from "./Header.module.scss";
 export const Header = () => {
   return (
     <div className={cls.header}>
-      <Link to={path.HOME} className={cls.header__icon}>
-        <img
-          className={cls.header__img}
-          src="/src/assets/icons/icon-logo.png"
-          alt="Опера Балет Театры"
-        />
-      </Link>
-      <ul className={cls.header__nav}>
-        {
-          HeaderList.map(({ id, caption, path }) => (
-            <li key={id} className={cls.header__nav__list}>
-              <NavLink
-                to={path}
-                className={cls.header__nav__item}
-              >
-                {caption}
-              </NavLink>
-            </li>
-          ))
-        }
-      </ul>
+      <div className={cls.header__container}>
+        <Link to={path.HOME} className={cls.header__icon}>
+          <img
+            className={cls.header__img}
+            src="/src/assets/icons/icon-logo.png"
+            alt="Опера Балет Театры"
+          />
+        </Link>
+        <ul className={cls.header__nav}>
+          {
+            HeaderList.map(({ id, caption, path }) => (
+              <li key={id} className={cls.header__nav__list}>
+                <NavLink
+                  to={path}
+                  className={cls.header__nav__item}
+                  activeClassName="active"
+                >
+                  {caption}
+                </NavLink>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   )
 };
