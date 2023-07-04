532eb2c5652abd3b7850fad2eb827f5581fbaba4
Experadog <lsstudent008@mail.ru>
2023-07-04 14:44:14 +0600

    add reset for index.scss

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -8,7 +8,21 @@ export const Footer = () => {
       <div className={cls.container}>
         <div className={cls.footer__top}>
           <div className={cls.footer__top__left}>
-
+            <div className={cls.footer__top__left__contacts}>
+              <h2>Контакты</h2>
+            </div>
+            <div className={cls.footer__top__left__elements}>
+              <ul>
+                <li>
+                  <img src="/src/assets/icons/telegram-icon.svg.svg" alt="" />
+                  <span>
+                    Абдрахманова 167. Бишкек
+                  </span>
+                </li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
           </div>
           <div className={cls.footer__top__right}>
             
