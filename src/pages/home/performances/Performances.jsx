7d3bff9deccc6a62c4cd5fcd97c0769fa4475a14
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-07-06 11:56:54 +0600

    finish performances

--- src/pages/home/performances/Performances.jsx
+++ src/pages/home/performances/Performances.jsx
@@ -9,13 +9,28 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 
 export const Performances = () => {
+  const breakpoints = {
+    // Брейкпоинт на ширине экрана >= 640px
+    10: {
+      slidesPerView: 1,
+    },
+    // Брейкпоинт на ширине экрана >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // Брейкпоинт на ширине экрана >= 1024px
+    1024: {
+      slidesPerView: 4,
+    }
+  };
+
   return (
     <div className={cls.performances}>
       <h2 className={cls.performances__title}>
         БЛИЖАЙШИЕ СПЕКТАКЛИ
       </h2>
       <Swiper
-        style={{ minWidth: 0 }}
+        breakpoints={breakpoints}
         modules={[Navigation, Pagination, A11y, Autoplay]}
         spaceBetween={8}
         slidesPerView={4}
